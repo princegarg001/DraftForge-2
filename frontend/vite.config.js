2a6db211ee09ce5872b319a793529e8dd490fd63
Himanshi Singla <singlahimanshi2@gmail.com>
2026-08-23 21:35:36 +0530

    made changes in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    allowedHosts: ['draftforge-2-1.onrender.com']
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: ['draftforge-2-1.onrender.com']
   }
 });
